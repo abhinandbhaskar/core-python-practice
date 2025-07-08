cb45707433c40bc46d69b5cd65880f99435c1681
Abhinand Bhaskar <abhinandbhaskar43@gmail.com>
2025-07-08 20:30:38 +0530

    Python Basics Refresh

---
+++ strings.py
@@ -0,0 +1,106 @@
+# print("Hello world...")
+
+# arr="Hello Guys"
+# print(arr[0])
+
+# str1="Hello World"
+# print(str1[0:])
+
+# str2="Malayalam"
+# print(str2[:3])
+
+
+'''Built-in String Functions and Methods'''
+
+# str1="AbhinandBhaskar"
+# print(len(str1))
+# print(len("Abhi"))
+
+# name="abhinand"
+# print(name.upper())
+
+# name2="ABHINAND"
+# print(name2.lower())
+
+# name="apple"
+# n=name.count("p")
+# print(n)
+
+# name="Apple"
+# print(name.find("e"))
+
+# str1="Have a nice day"
+# s=str1.replace("nice","good")
+# print(s)
+
+
+# str1="Hello Guys"
+# s=str1.replace("Hello","Hi")
+# print(s)
+
+# a="Have a"
+# b="Nice day"
+# print(a+" "+b)
+# print(a*3)
+
+''' Formatted Strings '''
+
+# a="Hello"
+# b="Abhi"
+# print("{} My Name is {}".format(a,b))
+
+
+# Input 
+
+# num=input("Enter a number : ")
+# print("You entered number is {}".format(num))
+
+
+# inp1=input("What is Your Name : ")
+# print("My name is {}".format(inp1))
+
+
+# num1=int(input("Enter number1 : "))
+# num2=int(input("Enter number2 : "))
+# print("Sum of {}+{}={}".format(num1,num2,num1+num2))
+
+
+# inp1=int(input("Enter a number : "))
+
+# if inp1%2==0:
+#     print("Entered number {} is even".format(inp1))
+# else:
+#     print("Entered number {} odd ".format(inp1))
+
+
+
+
+# inp=int(input("Enter Your Mark : "))
+# if inp >= 80 and inp <= 100 :
+#     print("You have god distinction")
+# elif inp >=60 and inp <= 80:
+#     print("You have got first class")
+# elif inp >= 40 and inp <= 80:
+#     print("You have god second class")
+# else:
+#     print("You have god third class")
+
+
+
+# inp=int(input("Enter a number : "))
+
+# print("Multiplication table for {}".format(inp))
+# i=1
+# while i<=10:
+#     print("{} x {} = {}".format(i,inp,i*inp))
+#     i+=1
+
+
+
+
+
+
+
+
+
+
